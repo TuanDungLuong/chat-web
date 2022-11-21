efc7f1b369ee432cde5ddd4bf90704ecf73a02d7
Lương Tuấn Dũng <luongtuandung181209@outlook.com>
2022-11-21 15:48:43 +0700

    Updates

---
+++ precache-manifest.e89c45957a8c1875ca0cd3695408b8cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c1976318446b612ffcb439bd6a8bc1",
+    "url": "/TuanDungLuong/chat-web.git/index.html"
+  },
+  {
+    "revision": "6cd87869268e50abd44d",
+    "url": "/TuanDungLuong/chat-web.git/static/css/main.9cb7495d.chunk.css"
+  },
+  {
+    "revision": "df28f011efe8c52d7cc4",
+    "url": "/TuanDungLuong/chat-web.git/static/js/2.ee37ebed.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/TuanDungLuong/chat-web.git/static/js/2.ee37ebed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cd87869268e50abd44d",
+    "url": "/TuanDungLuong/chat-web.git/static/js/main.c3bb2ff9.chunk.js"
+  },
+  {
+    "revision": "15a541d27b35ca10cd5a",
+    "url": "/TuanDungLuong/chat-web.git/static/js/runtime-main.1a81e035.js"
+  }
+]);
